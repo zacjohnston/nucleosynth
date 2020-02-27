920e73aef76661159b38462bd8de31a00f46fa2b
Zac Johnston <zac.johnston@monash.edu>
2020-02-27 17:02:59 -0500

    [paths.py] added function: skynet_path()

--- nucleosynth/paths.py
+++ nucleosynth/paths.py
@@ -24,15 +24,15 @@ def repo_path():
     return path
 
 
-# def progs_path():
-#     """Return path to top-level directory of progenitors
-#     """
-#     try:
-#         path = os.environ['PROGENITORS']
-#     except KeyError:
-#         raise EnvironmentError('Environment variable PROGENITORS not set. '
-#                                'Set path to progenitors top-level directory, e.g., '
-#                                "'export PROGENITORS=${HOME}/data/progenitors'")
-#     return path
+def skynet_path():
+    """Return path to skynet output
+    """
+    try:
+        path = os.environ['SKYNET_OUTPUT']
+    except KeyError:
+        raise EnvironmentError('Environment variable SKYNET_OUTPUT not set. '
+                               'Set path to skynet output directory, e.g., '
+                               "'export SKYNET_OUTPUT=${HOME}/skynet/output'")
+    return path
 
 
