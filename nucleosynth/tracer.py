54e0dfbadb238f57fb676548d14ee01de3db4e04
Zac Johnston <zac.johnston@monash.edu>
2020-03-02 17:25:56 -0500

    [tracer.py] starting Tracer class

--- nucleosynth/tracer.py
+++ nucleosynth/tracer.py
@@ -1,3 +1,19 @@
+
+# nucleosynth
+from . import network
+from . import load_save
+
 """
 Class representing an individual mass tracer from a model
 """
+
+
+class Tracer:
+    """
+    Object representing an individual mass tracer from a model
+    """
+    def __init__(self):
+        """
+
+        """
+        pass
