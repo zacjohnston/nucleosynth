64452f07ad2fe5e7a9891b1459af20a733eb2226
Zac Johnston <zac.johnston@monash.edu>
2020-03-02 17:45:48 -0500

    [tracer.py] added __init__()

--- nucleosynth/tracer.py
+++ nucleosynth/tracer.py
@@ -1,5 +1,6 @@
 
 # nucleosynth
+from . import paths
 from . import network
 from . import load_save
 
@@ -12,8 +13,17 @@ class Tracer:
     """
     Object representing an individual mass tracer from a model
     """
-    def __init__(self):
+    def __init__(self, tracer, model):
         """
+        parameters
+        ----------
+        tracer : int
+        model : str
+        """
+        self.tracer = tracer
+        self.model = model
+        self.path = paths.model_path(model=model)
 
-        """
-        pass
+        self.network = network.get_tracer_network(tracer=tracer, model=model)
+        self.abu = network.get_tracer_abu(tracer=tracer, model=model)
+        self.table = load_save.extract_tracer(tracer=tracer, model=model)
