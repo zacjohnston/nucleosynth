2eaa0348d041585dc6a61279a90a1c60f5535c1b
Zac Johnston <zacpjohnston@gmail.com>
2020-03-04 18:22:16 -0500

    [tracer.py] added attr file

--- nucleosynth/tracer.py
+++ nucleosynth/tracer.py
@@ -25,6 +25,7 @@ class Tracer:
         self.path = paths.model_path(model=model)
         self.verbose = verbose
 
+        self.file = load_save.load_tracer_file(tracer, model, verbose=verbose)
         self.network = network.get_tracer_network(tracer, model, verbose=verbose)
         self.abu = network.get_tracer_abu(tracer, model, verbose=verbose)
         self.table = load_save.load_tracer_columns(tracer, model, verbose=verbose)
