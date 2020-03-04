e404d6b9e90014212544fb0698b124c40791f82d
Zac Johnston <zacpjohnston@gmail.com>
2020-03-04 17:26:34 -0500

    [__init__.py] added printing.py

--- nucleosynth/__init__.py
+++ nucleosynth/__init__.py
@@ -2,9 +2,11 @@ from . import tracer
 from . import paths
 from . import load_save
 from . import network
+from . import printing
 
 __all__ = ['tracer',
            'paths',
            'load_save',
            'network',
+           'printing',
            ]
