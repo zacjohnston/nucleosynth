43685f3b20ee82961fd6e2f4673ae59f632240f7
Zac Johnston <zacpjohnston@gmail.com>
2020-03-04 17:45:05 -0500

    [load_save.py] gd

--- nucleosynth/load_save.py
+++ nucleosynth/load_save.py
@@ -4,20 +4,23 @@ import h5py
 
 # nucleosynth
 from . import paths
+from .printing import printv
 
 """
 Functions for loading/saving data
 """
 
 
-def extract_tracer(tracer, model):
+def extract_tracer(tracer, model, verbose=True):
     """Load skynet tracer hdf5 file
 
     parameters
     ----------
     tracer : int
     model : str
+    verbose : bool
     """
+    printv(f'Loading tracer table', verbose=verbose)
     table = pd.DataFrame()
     f = load_hdf5(tracer, model)
     keys = ['Time', 'Density', 'Temperature', 'Ye', 'HeatingRate', 'Entropy']
@@ -28,14 +31,16 @@ def extract_tracer(tracer, model):
     return table
 
 
-def load_tracer_network(tracer, model):
+def load_tracer_network(tracer, model, verbose=True):
     """Load isotope info (Z, A) for tracer
 
     parameters
     ----------
     tracer : int
     model : str
+    verbose : bool
     """
+    printv(f'Loading tracer network', verbose=verbose)
     table = pd.DataFrame()
     f = load_hdf5(tracer, model)
 
@@ -45,29 +50,33 @@ def load_tracer_network(tracer, model):
     return table
 
 
-def load_abu(tracer, model):
+def load_abu(tracer, model, verbose=True):
     """Load chemical abundance table from tracer file
 
     parameters
     ----------
     tracer : int
     model : str
+    verbose : bool
     """
+    printv(f'Loading tracer abundances', verbose=verbose)
     f = load_hdf5(tracer, model)
     abu = pd.DataFrame(f['Y'])
 
     return abu
 
 
-def load_hdf5(tracer, model):
+def load_hdf5(tracer, model, verbose=True):
     """Load skynet tracer hdf5 file
 
     parameters
     ----------
     tracer : int
     model : str
+    verbose : bool
     """
     filepath = paths.tracer_filepath(tracer, model=model)
-    f = h5py.File(filepath, 'r')
+    printv(f'Loading tracer hdf5: {filepath}', verbose=verbose)
 
+    f = h5py.File(filepath, 'r')
     return f
