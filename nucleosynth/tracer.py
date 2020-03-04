c0f300ea6eedf56604301d3d0ec54003efe35732
Zac Johnston <zacpjohnston@gmail.com>
2020-03-04 18:25:03 -0500

    [tracer.py] added method load_tracer_file()

--- nucleosynth/tracer.py
+++ nucleosynth/tracer.py
@@ -25,7 +25,13 @@ class Tracer:
         self.path = paths.model_path(model=model)
         self.verbose = verbose
 
-        self.file = load_save.load_tracer_file(tracer, model, verbose=verbose)
+        self.file = None
         self.network = network.get_tracer_network(tracer, model, verbose=verbose)
         self.abu = network.get_tracer_abu(tracer, model, verbose=verbose)
         self.table = load_save.load_tracer_columns(tracer, model, verbose=verbose)
+
+    def load_tracer_file(self):
+        """Load raw tracer file
+        """
+        self.file = load_save.load_tracer_file(self.tracer, self.model,
+                                               verbose=self.verbose)
