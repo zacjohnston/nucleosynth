e353ee39177f743d5ca515591b1bb538d64574ef
Zac Johnston <zacpjohnston@gmail.com>
2020-03-04 19:01:36 -0500

    [load_save.py] added arg tracer_network to load_tracer_network()

--- nucleosynth/load_save.py
+++ nucleosynth/load_save.py
@@ -40,36 +40,8 @@ def load_tracer_columns(tracer, model, columns=None, tracer_file=None,
     return table
 
 
-def load_tracer_network(tracer, model, tracer_file=None, verbose=True):
-    """Load isotope info (Z, A) used in tracer
-
-    parameters
-    ----------
-    tracer : int
-    model : str
-    tracer_file : h5py.File
-    verbose : bool
-    """
-    printv(f'Loading tracer network', verbose=verbose)
-
-    tracer_file = load_tracer_file(tracer, model, tracer_file, verbose=verbose)
-    tracer_network = pd.DataFrame()
-    iso_list = []
-
-    for key in ['Z', 'A']:
-        tracer_network[key] = np.array(tracer_file[key], dtype=int)
-
-    for i in range(len(tracer_network)):
-        row = tracer_network.loc[i]
-        iso_str = network.get_isotope_str(z=row['Z'], a=row['A'])
-        iso_list += [iso_str]
-
-    tracer_network['isotope'] = iso_list
-
-    return tracer_network[['isotope', 'Z', 'A']]
-
-
-def load_tracer_abu(tracer, model, tracer_file=None, verbose=True):
+def load_tracer_abu(tracer, model, tracer_file=None, tracer_network=None,
+                    verbose=True):
     """Load chemical abundance table from tracer file
 
     parameters
@@ -77,6 +49,7 @@ def load_tracer_abu(tracer, model, tracer_file=None, verbose=True):
     tracer : int
     model : str
     tracer_file : h5py.File
+    tracer_network : pd.DataFrame
     verbose : bool
     """
     printv(f'Loading tracer abundances', verbose=verbose)
@@ -88,6 +61,40 @@ def load_tracer_abu(tracer, model, tracer_file=None, verbose=True):
     tracer_abu.columns = list(tracer_network['isotope'])
 
     return tracer_abu
+
+
+def load_tracer_network(tracer, model, tracer_file=None, tracer_network=None,
+                        verbose=True):
+    """Load isotope info (Z, A) used in tracer
+
+    parameters
+    ----------
+    tracer : int
+    model : str
+    tracer_file : h5py.File
+    tracer_network : pd.DataFrame
+    verbose : bool
+    """
+    printv(f'Loading tracer network', verbose=verbose)
+
+    tracer_file = load_tracer_file(tracer, model, tracer_file, verbose=verbose)
+    
+    if tracer_network is None:
+        tracer_network = pd.DataFrame()
+        iso_list = []
+
+        for key in ['Z', 'A']:
+            tracer_network[key] = np.array(tracer_file[key], dtype=int)
+
+        for i in range(len(tracer_network)):
+            row = tracer_network.loc[i]
+            iso_str = network.get_isotope_str(z=row['Z'], a=row['A'])
+            iso_list += [iso_str]
+
+        tracer_network['isotope'] = iso_list
+        tracer_network = tracer_network[['isotope', 'Z', 'A']]
+
+    return tracer_network
 
 
 def load_tracer_file(tracer, model, tracer_file=None, verbose=True):
