10551d9bd8b330ca5466c71d3dc4a371fefc4c0a
Zac Johnston <zacpjohnston@gmail.com>
2020-03-05 16:24:20 -0500

    [config/plot_config.py] added ax_labels

--- nucleosynth/config/plot_config.py
+++ nucleosynth/config/plot_config.py
@@ -6,3 +6,12 @@ ax_scales = {
     'entropy': 'linear',
     'time': 'linear',
 }
+
+ax_labels = {
+    'density': r'$\rho$ (g/cm^3)',
+    'temperature': '$T$ (K)',
+    'heatingrate': 'Heating Rate',  # TODO
+    'ye': '$Y_e$',
+    'entropy': '$S$',  # TODO
+    'time': 'Time (s)',
+}
