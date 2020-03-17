ea5e4ee2f97ca543e6f1bb5665264f0654ba8809
Zac Johnston <zacpjohnston@gmail.com>
2020-03-17 12:06:27 -0400

    [model.py] starting basic init

--- nucleosynth/models/model.py
+++ nucleosynth/models/model.py
@@ -1,7 +1,7 @@
 
 # nucleosynth
 from nucleosynth.tracers import tracer
-from nucleosynth import paths
+from nucleosynth import paths, tools
 
 """
 Class representing a CCSN model, composed of multiple mass tracers
@@ -14,13 +14,26 @@ class Model:
     attributes
     ----------
     model : str
+        Name of skynet model, e.g. 'traj_s12.0'
     path : str
+        Path to skynet output directory of model
+    tracers : {tracer_id: Tracer}
+        Collection of tracer objects
+    tracer_steps : [int]
+        list of skynet "steps" making up full tracer evolution
     """
-    def __init__(self, model):
+
+    def __init__(self, model, tracer_ids, tracer_steps=(1, 2)):
         """
         parameters
         ----------
         model : str
+        tracer_ids : int or [int]
+            list of tracers. If int: assume tracer_ids = [0..(int-1)]
         """
         self.model = model
         self.path = paths.model_path(model=model)
+        self.tracer_steps = tracer_steps
+
+        tracer_ids = tools.expand_sequence(tracer_ids)
+        self.tracers = dict.fromkeys(tracer_ids)
