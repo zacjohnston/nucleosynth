11191c07693948ca9e9626aca5b332e8afb6fd4c
Zac Johnston <zacpjohnston@gmail.com>
2020-03-17 14:56:44 -0400

    [__init__.py] updated

--- nucleosynth/__init__.py
+++ nucleosynth/__init__.py
@@ -1,9 +1,11 @@
 from . import tracers
+from . import models
 from . import paths
 from . import network
 from . import printing
 
 __all__ = ['tracers',
+           'models',
            'paths',
            'network',
            'printing',
