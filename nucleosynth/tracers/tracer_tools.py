03d46f343a5a179efcb1720453f0e1d8957f37d9
Zac Johnston <zacpjohnston@gmail.com>
2020-05-11 17:31:15 -0400

    [tracer_tools.py] added get_total_heating()

--- nucleosynth/tracers/tracer_tools.py
+++ nucleosynth/tracers/tracer_tools.py
@@ -1,1 +1,18 @@
 import numpy as np
+
+"""
+General tools for tracer data
+"""
+
+
+def get_total_heating(table):
+    """Calculate total heating from 'columns' table
+        by integrating heatingrate over time.
+
+    parameters
+    ----------
+    table : pd.DataFrame
+        table containing tracer columns of 'time' and 'heatingrate'
+    """
+    total_heating = np.trapz(y=table['heatingrate'], x=table['time'])
+    return total_heating
